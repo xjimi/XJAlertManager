30bd7525510e237638dad90cf6d4436c4de7f9bb
xjimi <fn5128@gmail.com>
2014-11-21 16:51:23 +0800

    Initial Commit

---
+++ XJAlertManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJAlertManagerDemo
+//
+//  Created by jimi on 2014/11/21.
+//  Copyright (c) 2014年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJAlertManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJAlertManagerDemo
+//
+//  Created by jimi on 2014/11/21.
+//  Copyright (c) 2014年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
